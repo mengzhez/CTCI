c436d1e4a4a7df797fb1e1d7b8e4fb9dcc9653ea
mengzhez <dreamfulsage@gmail.com>
2015-04-18 17:15:12 -0400

    1-1 Unique characters

--- Ch1/1-1.cpp
+++ Ch1/1-1.cpp
@@ -1,8 +1,25 @@
 #include<iostream>
+#include<unordered_set>
 using namespace std;
+
+bool UniqueCharacters(string s)
+{
+	int length = s.size();
+	unordered_set<char> set;
+	for(int i = 0; i < length; i++)
+	{
+		if (set.count(s[i]) > 0)
+			return false;
+		else
+			set.insert(s[i]);
+	}
+
+	return true;
+}
 
 void main()
 {
-	cout << "Hello world" << "\n";
+	bool b = UniqueCharacters("abcdefghijklmnopqrstuvwxyz!");
+	cout << (b ? "true" : "false") << "\n";
 	system("pause");
 }
