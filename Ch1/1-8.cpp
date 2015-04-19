1868465d73bb102fe83880af67aa1a7f61852329
mengzhez <dreamfulsage@gmail.com>
2015-04-19 12:08:27 -0400

    Is rotation

---
+++ Ch1/1-8.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+bool isSubstring(string str, string sub)
+{
+	if (str.size() < sub.size())
+		return false;
+
+	if (str.find(sub) != string::npos)
+		return true;
+	else
+		return false;
+}
+
+bool isRotation(string str1, string str2)
+{
+	if (str1.size() != str2.size())
+		return false;
+
+	string temp = str2 +str2;
+	return isSubstring(temp, str1);
+}
+
+void main()
+{
+	string str1 = "waterbottle";
+	string str2 = "erbottlewat";
+	bool b = isRotation(str1, str2);
+	cout << (b ? "true" : "false") << "\n";
+	system("pause");
+}
