502f17f74b81110b3af3f2f0f5de58a6cb748d6e
mengzhez <dreamfulsage@gmail.com>
2015-04-18 22:02:22 -0400

    Is permutation

--- Ch1/1-3.cpp
+++ Ch1/1-3.cpp
@@ -2,24 +2,24 @@
 #include<unordered_set>
 using namespace std;
 
-bool isPermutation(string s1, string s2)
+bool isPermutation(string str1, string str2)
 {
-	int length1 = s1.size();
-	int length2 = s2.size();
+	int n1 = str1.size();
+	int n2 = str2.size();
 
-	if (length1 != length2)
+	if (n1 != n2)
 		return false;
 
 	unordered_set<char> set;
-	for (int i = 0; i < length1; i++)
-		set.insert(s1[i]);
+	for (int i = 0; i < n1; i++)
+		set.insert(str1[i]);
 
-	for (int i = 0; i < length2; i++)
+	for (int i = 0; i < n2; i++)
 	{
-		if (set.count(s2[i]) == 0)
+		if (set.count(str2[i]) == 0)
 			return false;
 
-		set.erase(s2[i]);
+		set.erase(str2[i]);
 	}
 
 	if (set.size() == 0)
@@ -30,9 +30,9 @@ bool isPermutation(string s1, string s2)
 
 void main()
 {
-	string s1 = "abcdefa";
-	string s2 = "cfgabde";
-	bool b = isPermutation(s1, s2);
+	string str1 = "abcdefa";
+	string str2 = "cfgabde";
+	bool b = isPermutation(str1, str2);
 	cout << (b ? "true" : "false") << "\n";
 	system("pause");
 }
