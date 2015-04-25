1cfd38880b77c49fa807f2727fd350e3d8ddec5b
mengzhez <dreamfulsage@gmail.com>
2015-04-25 00:38:39 -0400

    Implement queue using two stacks

---
+++ Ch3/3-5.cpp
@@ -0,0 +1,68 @@
+#include<iostream>
+#include<stack>
+
+using namespace std;
+
+template <class Type> class MyQueue
+{
+private:
+	stack<Type> _s1;
+	stack<Type> _s2;
+
+public:
+	MyQueue()
+	{}
+
+	~MyQueue()
+	{}
+
+	void push(Type n)
+	{
+		while (!_s1.empty())
+		{
+			_s2.push(_s1.top());
+			_s1.pop();
+		}
+
+		_s2.push(n);
+		while (!_s2.empty())
+		{
+			_s1.push(_s2.top());
+			_s2.pop();
+		}
+	}
+
+	void pop()
+	{
+		if (!_s1.empty())
+			_s1.pop();
+	}
+
+	Type front()
+	{
+		return _s1.top();
+	}
+
+	bool empty()
+	{
+		return _s1.empty();
+	}
+};
+
+void main()
+{
+	MyQueue<int> q;
+	q.push(1);
+	q.push(2);
+	q.push(3);
+
+	cout << q.front() << "\n";
+	q.pop();
+
+	cout << q.front() << "\n";
+	q.pop();
+	q.pop();
+
+	cout << (q.empty() ? "true" : "false") << "\n";
+	system("pause");
+}
