b52821fa16614116e8113a483ce40246600c6927
mengzhez <dreamfulsage@gmail.com>
2015-04-26 00:51:19 -0400

    Reverse the string

--- Ch1/1-2.cpp
+++ Ch1/1-2.cpp
@@ -1,16 +1,20 @@
 #include<iostream>
 using namespace std;
 
+// Reverse the string 
 void reverse(char* str)
 {
 	int n = 0;
 	char* p = str;
+	
+	// Count the length
 	while (*p)
 	{
 		n++;
 		p++;
 	}
 
+	// Swap the symmetric elements
 	for (int i = 0; i < (n / 2); i++)
 	{
 		char temp = str[i];
