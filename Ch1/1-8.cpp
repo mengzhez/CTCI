7a186a12538794d7ee83788f77a73f0ad1f3e837
mengzhez <dreamfulsage@gmail.com>
2015-04-26 00:52:15 -0400

    Check if one string is a rotation of the other

--- Ch1/1-8.cpp
+++ Ch1/1-8.cpp
@@ -2,6 +2,7 @@
 #include<string>
 using namespace std;
 
+// Check if one string is the substring of the other
 bool isSubstring(string str, string sub)
 {
 	if (str.size() < sub.size())
@@ -13,11 +14,13 @@ bool isSubstring(string str, string sub)
 		return false;
 }
 
+// Check if one string is a rotation of the other
 bool isRotation(string str1, string str2)
 {
 	if (str1.size() != str2.size())
 		return false;
 
+	// Concatenate two strings and check the substring
 	string temp = str2 +str2;
 	return isSubstring(temp, str1);
 }
