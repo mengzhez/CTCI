fd2034839c2374c4eb3507702a7a17d46817be26
mengzhez <dreamfulsage@gmail.com>
2015-04-26 00:51:29 -0400

    Check if one string is the permutation of the other

--- Ch1/1-3.cpp
+++ Ch1/1-3.cpp
@@ -2,6 +2,7 @@
 #include<unordered_set>
 using namespace std;
 
+// Check if one string is the permutation of the other
 bool isPermutation(string str1, string str2)
 {
 	int n1 = str1.size();
@@ -10,10 +11,12 @@ bool isPermutation(string str1, string str2)
 	if (n1 != n2)
 		return false;
 
+	// Use Hash-table to store all characters
 	unordered_set<char> set;
 	for (int i = 0; i < n1; i++)
 		set.insert(str1[i]);
 
+	// Check if all the characters in the others string appear in the table
 	for (int i = 0; i < n2; i++)
 	{
 		if (set.count(str2[i]) == 0)
