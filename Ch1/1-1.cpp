b86dc06342bbae0f0c639b0de86c6211125a76b8
mengzhez <dreamfulsage@gmail.com>
2015-04-26 00:51:08 -0400

    Check if all the characters are unique

--- Ch1/1-1.cpp
+++ Ch1/1-1.cpp
@@ -2,8 +2,11 @@
 #include<unordered_set>
 using namespace std;
 
+// Check if all the characters are unique
+// Didn't check whether the character is ASCII or Unicode
 bool uniqueCharacters(string str)
 {
+	// Use unordered_set to store characters, if one character appears twice, return false
 	int n = str.size();
 	unordered_set<char> set;
 	for(int i = 0; i < n; i++)
