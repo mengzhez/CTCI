108412c11506d7292eab164a7a2a1c3847a9c9c5
mengzhez <dreamfulsage@gmail.com>
2015-04-29 22:22:55 -0400

    Find the missing number in a[]

---
+++ Ch5/5-7.cpp
@@ -0,0 +1,52 @@
+#include<iostream>
+
+using namespace std;
+
+// Fetch the jth bit of the ith integer in array a
+int fetchBit(int a[], int i, int j)
+{
+	return (a[i] & (1 << j)) != 0;
+}
+
+// Fetch the jth bit of the integer n
+int fetchBit(int n, int j)
+{
+	return (n & (1 << j)) != 0;
+}
+
+int findMissingNumber(int a[], int n)
+{
+	int x = 0;
+	for (int i = 0; i < n; i++)
+	{
+		// Assume integer has 32 bits
+		for (int j = 0; j < 31; j++)
+		{
+			int b = fetchBit(x, j);
+			b ^= fetchBit(a, i, j);
+			b ^= fetchBit(i, j);
+			x &= (~(1 << j));
+			x |= (b << j);
+		}
+	}
+
+	for (int j = 0; j < 31; j++)
+	{
+		int b = fetchBit(x, j);
+		b ^= fetchBit(n, j);
+		x &= (~(1 << j));
+		x |= (b << j);
+	}
+
+	return x;
+}
+
+void main()
+{
+	int n = 10;
+	int a[] = {0, 1, 2, 3, 4, 5, 6, 7, 9, 10};
+	int m = findMissingNumber(a, n);
+	cout << m << "\n";
+	
+	system("pause");
+}
