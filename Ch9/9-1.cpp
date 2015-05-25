cec0babe5427562c55c27383f5e5a84c4c12b57c
mengzhez <dreamfulsage@gmail.com>
2015-05-24 22:45:14 -0400

    Count the number of ways for the child to run up a staircase with n steps

---
+++ Ch9/9-1.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+// Count the number of ways for the child to run up a staircase with n steps
+// The child can hop either one, two or three steps
+int countWays(int n)
+{
+	if (n < 0)
+		return 0;
+	else if (n == 0 || n == 1)
+		return 1;
+	else
+		return (countWays(n - 1) + countWays(n - 2) + countWays(n - 3));
+}
+
+void main()
+{
+	cout << countWays(4) << endl;
+	system("pause");
+}
