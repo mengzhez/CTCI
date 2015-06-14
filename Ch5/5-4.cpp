0a914a1e2656eca5abacf3f1fbe7ef6f265d9d12
mengzhez <dreamfulsage@gmail.com>
2015-06-14 17:06:50 -0400

    (n & (n - 1)) == 0

--- Ch5/5-4.cpp
+++ Ch5/5-4.cpp
@@ -6,8 +6,9 @@ using namespace std;
 void main()
 {
 	// ((n & (n - 1)) == 0)
-	// (n - 1) is to make the lowest 1 of n become 0
+	// (n - 1) will make the least significant 1 to 0 and the 0s after it to 1s
 	// This is used to check if n has only one '1' in the binary representation
+	// or if n is a power of 2
 
 	int n = 8; // 00001000
 	bool b = ((n & (n - 1)) == 0);
