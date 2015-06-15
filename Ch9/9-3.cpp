a05be35fe05e7111b37e38ab6e8afd6dd46dd0a1
mengzhez <dreamfulsage@gmail.com>
2015-06-14 23:38:57 -0400

    Find the magic index which satisfies a[i] = i

--- Ch9/9-3.cpp
+++ Ch9/9-3.cpp
@@ -5,18 +5,34 @@ using namespace std;
 // Find the magic index which satisfies a[i] = i
 // Assume that if the magic index exists, only one exists in the input array. If it doesn't exist, return -1
 // Current algorithm is not efficient enough
-int findMagicIndex(vector<int> a)
+int findMagicIndex(vector<int> a, int low, int high)
 {
-	int index = 0;
-	while (index < a.size())
-	{
-		if (a[index] == index)
-			return index;
+	if (low > high)
+		return -1;
 
-		if ((a[index] - index) > 0)
-			index = a[index];
-		else
-			index++;
+	int mid = (low + high) / 2;
+	if (a[mid] == mid)
+		return mid;
+
+	if (a[mid] > mid)
+	{
+		int left = findMagicIndex(a, low, mid - 1);
+		if (left >= 0)
+			return left;
+
+		int right = findMagicIndex(a, max(a[mid], mid), high);
+		if (right >= 0)
+			return right;
+	}
+	else
+	{
+		int left = findMagicIndex(a, low, min(a[mid], mid));
+		if (left >= 0)
+			return left;
+
+		int right = findMagicIndex(a, mid + 1, high);
+		if (right >= 0)
+			return right;
 	}
 
 	return -1;
@@ -35,7 +51,7 @@ void main()
 	a.push_back(12);
 	a.push_back(12);
 
-	int index = findMagicIndex(a);
+	int index = findMagicIndex(a, 0, a.size() - 1);
 	cout << index << endl;
 	system("pause");
 }
