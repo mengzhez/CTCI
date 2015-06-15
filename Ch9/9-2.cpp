0bb8d7ad4cbd49eb4083aa57d30150935974528a
mengzhez <dreamfulsage@gmail.com>
2015-06-14 23:38:39 -0400

    Count the number of possible path for the robot to move from (0, 0) to (x, y)

--- Ch9/9-2.cpp
+++ Ch9/9-2.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 // Count the number of possible path for the robot to move from (0, 0) to (x, y)
 // Assume we have a 5x5 map. In the map, 1 means possible path, 0 means obstacles
-int countPaths(int map[][5], int cx, int cy, int tx, int ty)
+int countPaths(int map[][5], int flag[][5], int cx, int cy, int tx, int ty)
 {
 	if (cx > tx || cy > ty)
 		return 0;
@@ -14,7 +14,12 @@ int countPaths(int map[][5], int cx, int cy, int tx, int ty)
 	if (cx == tx && cy == ty)
 		return 1;
 
-	return countPaths(map, cx + 1, cy, tx, ty) + countPaths(map, cx, cy + 1, tx, ty);
+	int count1 = (flag[cx + 1][cy] != 0) ? flag[cx + 1][cy] : countPaths(map, flag, cx + 1, cy, tx, ty);
+	flag[cx + 1][cy] = count1;
+	int count2 = (flag[cx][cy + 1] != 0) ? flag[cx][cy + 1] : countPaths(map, flag, cx, cy + 1, tx, ty);
+	flag[cx][cy + 1] = count2;
+
+	return count1 + count2;
 }
 
 void main()
@@ -27,6 +32,8 @@ void main()
 		{ 1, 1, 1, 1, 1 }
 	};
 
-	cout << countPaths(map, 0, 0, 3, 1) << endl;
+	int flag[5][5] = {0};
+
+	cout << countPaths(map, flag, 0, 0, 3, 1) << endl;
 	system("pause");
 }
